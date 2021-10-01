c4c222ab716115f1cf4875e56b724b7a92034ab9
Danny Roubin <dannyroubin1@gmail.com>
2021-09-30 17:04:19 -0700

    initialized file and added imports and main method

---
+++ Fractions_V1.java
@@ -0,0 +1,26 @@
+/*
+Name: Danny Roubin
+Class: CSS 143 Sec B
+Assignment: Fractions V1
+*/
+
+import java.util.Scanner;
+import java.io.PrintWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+
+public class Fractions_V1
+{
+
+    //shell that will run the methods
+    public static void main(String[] args)
+    {
+        int reRun;
+        // do {
+        //     // runner();
+        //     // reRun = reRun();
+        // }while (reRun != 1);
+    }
+
+}
